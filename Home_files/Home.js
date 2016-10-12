dd9b0f26cf7b5f4b8ada6523b33668a4d8b0506a
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-10-12 21:50:56 +0500

    first Commit

---
+++ Home_files/Home.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20151206
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
